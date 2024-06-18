0db55528e46068f2df6ff64857ad682c8127aa46
WANG Xuerui <git@xen0n.name>
2024-06-18 17:15:54 +0800

    tests: add a test case checking magic methods forwarding

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,3 +1,5 @@
+import functools
+
 import xingque
 
 
@@ -58,3 +60,91 @@ ab = a * b
 
     ab = get_module("ab.star")
     assert ab.get("ab") == 42
+
+
+def test_magic_method_forwarding():
+    @functools.total_ordering
+    class Foo:
+        def __init__(self) -> None:
+            self.methods_called: list[str] = []
+
+        def __bool__(self) -> bool:
+            self.methods_called.append("bool")
+            return False
+
+        def __eq__(self, other: object) -> bool:
+            self.methods_called.append("eq")
+            return other == 123
+
+        def __lt__(self, other: int) -> bool:
+            self.methods_called.append("lt")
+            return other < 123
+
+        def __len__(self) -> int:
+            self.methods_called.append("len")
+            return 111222
+
+        def __contains__(self, item: object) -> bool:
+            self.methods_called.append("contains")
+            return item == 123
+
+        def __call__(self, foo: int, bar: str) -> object:
+            self.methods_called.append("call")
+            assert foo == 123
+            assert bar == "baz"
+            return {"ok": True, "test": ["aaa", "bbb", "ccc"]}
+
+    text = """
+to_bool = bool(foo)
+eq = foo == 123
+ne = foo != 123
+le = foo <= 123
+lt = foo < 123
+ge = foo >= 123
+gt = foo > 123
+
+length = len(foo)
+contains = 123 in foo
+invoke = foo(123, bar='baz')
+
+# TODO
+#getitem = foo.test
+#add = foo + 123
+#sub = foo - 123
+#mul = foo * 123
+#div = foo / 123
+#mod = foo % 123
+"""
+
+    recorder = Foo()
+
+    am = xingque.AstModule.parse("test.star", text)
+    gb = xingque.GlobalsBuilder.standard()
+    gb.set("foo", recorder)
+    g = gb.build()
+    m = xingque.Module()
+    e = xingque.Evaluator(m)
+    e.eval_module(am, g)
+
+    assert recorder.methods_called == [
+        "bool",
+        "eq",
+        "eq",
+        "eq",
+        "eq",
+        "eq",
+        "eq",
+        "len",
+        "contains",
+        "call",
+    ]
+    assert not m.get("to_bool")
+    assert m.get("eq")
+    assert not m.get("ne")
+    assert m.get("le")
+    assert not m.get("lt")
+    assert m.get("ge")
+    assert not m.get("gt")
+    assert m.get("length") == 111222
+    assert m.get("contains")
+    assert m.get("invoke") == {"ok": True, "test": ["aaa", "bbb", "ccc"]}
